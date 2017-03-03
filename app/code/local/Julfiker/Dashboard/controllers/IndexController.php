d784c9b2eb943754df07feeb8a7f925dbbccc1a9
julfiker <infro.jewel@gmail.com>
2017-03-04 02:24:54 +0600

    Dashboard Back office Embedded with magento

---
+++ app/code/local/Julfiker/Dashboard/controllers/IndexController.php
@@ -0,0 +1,10 @@
+<?php
+
+class Julfiker_Dashboard_IndexController extends Mage_Core_Controller_Front_Action
+{
+    public function indexAction()
+    {
+        $this->loadLayout();
+        $this->renderLayout();
+    }
+}
