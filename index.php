b3c894fadcf3f98e743eff7b8c74683315ca5781
Julfiker <infro.jewel@gmail.com>
2017-04-07 12:03:37 +0600

    Error report

--- index.php
+++ index.php
@@ -23,8 +23,7 @@
  * @copyright  Copyright (c) 2006-2015 X.commerce, Inc. (http://www.magento.com)
  * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
+error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED);
 
 if (version_compare(phpversion(), '5.3.0', '<')===true) {
     echo  '<div style="font:12px/1.35em arial, helvetica, sans-serif;">
