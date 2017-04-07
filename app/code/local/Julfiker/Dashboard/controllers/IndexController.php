2adacc40fc8668dda60af57534c1cf42108bbc6c
Julfiker <infro.jewel@gmail.com>
2017-04-07 11:47:22 +0600

    Logic implements for access only for ambassador

--- app/code/local/Julfiker/Dashboard/controllers/IndexController.php
+++ app/code/local/Julfiker/Dashboard/controllers/IndexController.php
@@ -5,6 +5,26 @@ class Julfiker_Dashboard_IndexController extends Mage_Core_Controller_Front_Acti
     public function indexAction()
     {
         $this->loadLayout();
+
+        Mage::getSingleton('core/session', array('name' => 'frontend'));
+
+        $sessionCustomer = Mage::getSingleton("customer/session");
+
+        if($sessionCustomer->isLoggedIn()) {
+            $websitecode = Mage::app()->getWebsite()->getCode();
+            $currentAmbassadorCode = Mage::getSingleton('core/session')->getAmbassadorCode();
+            if ($websitecode == $currentAmbassadorCode) {
+                $params = $this->getRequest()->getParams();
+                $key = ($params) ? key($params) : "";
+                $this->getLayout()->getBlock('backoffice')->setKey($key);
+            }
+           else {
+               Mage::app()->getFrontController()->getResponse()->setRedirect(Mage::getUrl('customer/account'));
+           }
+         } else {
+            Mage::app()->getFrontController()->getResponse()->setRedirect(Mage::getUrl('customer/account/login'));
+        }
+
         $this->renderLayout();
     }
 }
