63e31da9d6c021a9c67833838f4d4cd3abf951ed
tarikuli@gmail.com <tarikuli@gmail.com>
2017-05-08 18:21:42 -0400

    Oms Added

--- app/code/local/Infinite/MagentoAPI/Model/Observer.php
+++ app/code/local/Infinite/MagentoAPI/Model/Observer.php
@@ -78,6 +78,9 @@ class Infinite_MagentoAPI_Model_Observer
 		$orderIds = $observer->getOrderIds();
 		$apiHelper = Mage::helper('magento_api/api');
 		$apiHelper->purchase($orderIds);
+		
+		$apiHelper = Mage::helper('magento_api/oms');
+		$apiHelper->pushPurchaseToOms($orderIds);
 	}
 
 	public function enableAddressFieldsToRegister($observer)
