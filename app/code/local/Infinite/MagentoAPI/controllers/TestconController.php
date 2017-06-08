1c6be2dbf3e03760c9fe6bc1bc7245257a8d6753
tarikuli@gmail.com <tarikuli@gmail.com>
2017-06-08 13:48:12 -0400

    ." URL will be call"

--- app/code/local/Infinite/MagentoAPI/controllers/TestconController.php
+++ app/code/local/Infinite/MagentoAPI/controllers/TestconController.php
@@ -114,7 +114,7 @@ class Infinite_MagentoAPI_TestconController extends Mage_Core_Controller_Front_A
 // 			echo "<br>". $customer->getEmail()."  ---  ".$customer->getFirstname()." ".$customer->getLastname();
 // 			$apiHelper = Mage::helper('magento_api/api');
 			
-			echo "<br>".$apiHelper->_getApiUrl()." URL will be call";
+			echo "<br> URL will be call".$apiHelper->_getApiUrl();
 			$params = array(
 				'username' => base64_decode($memberParams->username),
 				'password' => base64_decode($memberParams->password),
